0fddcc83b1dd76c0780628a4da6bf8547fb45f54
juniardys <juniardysetio@gmail.com>
2024-10-27 22:24:42 +0700

    setup boilerplate

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TransformInterceptor } from './interceptors/transform/transform.interceptor';
+import { ResponseMessage } from './decorators/response/response.decorator';
 
+@UseInterceptors(TransformInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ResponseMessage('Success!')
   getHello(): string {
     return this.appService.getHello();
   }
